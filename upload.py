e730738eae6594241ecd9d7897b77d3f9125cbdf
Ondrej Telka <ondrejtelka@gmail.com>
2021-02-19 17:40:48 +0100

    Update arg list

--- upload.py
+++ upload.py
@@ -6,15 +6,15 @@ from youtube_uploader_selenium import Video, YouTubeUploader
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--video",
-	                    help='Path to the video file',
-	                    required=True)
+	parser.add_argument("--video", required=True, help="path to the video file")
+	parser.add_argument("--channel", required=True, help="ID of the YouTube channel")
 	parser.add_argument("--title")
 	parser.add_argument("--description")
 	parser.add_argument("--playlist")
-	parser.add_argument("--privacy", help="public | unlisted | private")
-	parser.add_argument("--username", help="Google Account username")
-	parser.add_argument("--password", help="Google Account password")
+	parser.add_argument("--privacy", choices=["public", "unlisted", "private"], default="private")
+	parser.add_argument("--headless", dest="headless", action="store_true", help="log in without browser interaction")
+	parser.add_argument("--username", help="ČVUT username")
+	parser.add_argument("--password", help="ČVUT password")
 	args = parser.parse_args()
 	
 	video = Video(args.video, args.title, args.description, args.playlist, args.privacy)
