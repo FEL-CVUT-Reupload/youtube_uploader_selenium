7454a413f9f89fe233f300a3be9cdcf0c4353fd1
Ondrej Telka <ondrejtelka@gmail.com>
2021-03-11 01:27:12 +0100

    Create login.py (used for generating the login cookie)

---
+++ login.py
@@ -0,0 +1,18 @@
+import argparse
+
+from youtube_uploader_selenium import YouTubeUploader
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--channel", required=True, help="ID of the YouTube channel")
+	parser.add_argument("--headless", dest="headless", action="store_true", help="log in without browser interaction")
+	parser.add_argument("--username", required=True, help="ČVUT username")
+	parser.add_argument("--password", required=True, help="ČVUT password")
+	args = parser.parse_args()
+	
+	uploader = YouTubeUploader(args.headless, args.channel)
+	success = uploader.login(args.username, args.password)
+	
+	if not success:
+		exit(7)
