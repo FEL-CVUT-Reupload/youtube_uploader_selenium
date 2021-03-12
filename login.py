3ed1ce05b0dd3c0761a5e92fe93cde727a0da195
Ondrej Telka <ondrejtelka@gmail.com>
2021-03-12 01:46:35 +0100

    Make the --username and --password options optional

--- login.py
+++ login.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--channel", required=True, help="ID of the YouTube channel")
 	parser.add_argument("--headless", dest="headless", action="store_true", help="log in without browser interaction")
-	parser.add_argument("--username", required=True, help="ČVUT username")
-	parser.add_argument("--password", required=True, help="ČVUT password")
+	parser.add_argument("--username", help="ČVUT username")
+	parser.add_argument("--password", help="ČVUT password")
 	parser.add_argument("--cookies", help="path to the directory where cookies should be saved")
 	args = parser.parse_args()
 	
